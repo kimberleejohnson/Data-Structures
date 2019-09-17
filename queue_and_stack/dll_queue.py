0d3bddef233cce1417a370d79e63475621ae9003
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-17 08:27:46 -0700

    Used doubly_linked_list methods to add missing queue methods

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,16 +5,31 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
   def __init__(self):
     self.size = 0
-    # Why is our DLL a good choice to store our elements?
+    # DLL to store our elements because... 
+    self.storage = DoublyLinkedList() 
     
 
   # Insertion is "enqueue" 
+  # To add an item in a queue, we need to add that item to the front of the end of the list (FILO)
   def enqueue(self, value):
-    pass
+    # Increase length of list by 1
+    self.size += 1
+    # Add new item to back of list
+    self.storage.add_to_tail(value)
   
   # Deletion is "dequeue"
+  # To dequeue an item, we'll want to remove the item from the front of the last (FILO)
   def dequeue(self):
-    pass
+    # As long as the queue is not length of 0
+    if self.size > 0:
+      # Decrement the length of the queue
+      self.size -= 1
+      head_item = self.storage.head
+      self.storage.remove_from_head()
+      return head_item.value 
+    else: 
+      return None 
 
+  # Returns the length of the queue
   def len(self):
-    pass
+    return self.size 
